1b6bedacb354e8713e6257b5a1bf9fd920feebfd
Retostephen <retostephen2006@gmail.com>
2025-08-01 00:17:10 +0100

    I added a condition to task two that'll print if the inputs are invalid

--- functions.py
+++ functions.py
@@ -30,19 +30,24 @@ is_even()
 #Task 2: Ask the user for two numbers and calculate their sum
 def is_sum():
 	while True:
-		user_input1 = input("Please enter any number: ")
+		try:
+			user_input1 = input("Please enter any number: ")
 	
-		user_input2 = input("Please enter any number: ")
+			user_input2 = input("Please enter any number: ")
 	
-		number1 = int(user_input1)
+			number1 = int(user_input1)
 	
-		number2 = int(user_input2)
+			number2 = int(user_input2)
 	
-		total_sum = number1 + number2
-		print(f"The total sum of {number1} and {number2} is {total_sum}.")
+			total_sum = number1 + number2
+			print(f"The total sum of {number1} and {number2} is {total_sum}.")
+		except ValueError:
+			print("The value of the inputs are invalid")
 is_sum()
 '''
 
+
+'''
 #Task 3: Ask the user to input a number and find determine if it's positive, negative or zero
 def check_number_sign():
 	while True:
@@ -61,7 +66,4 @@ def check_number_sign():
 			print(f"The value {user_input} is not a valid number.")
 
 check_number_sign() 
-
-
-
-
+'''
