845d3cf3987632a5d69b0141e10e848d1d75f7d4
Retostephen <retostephen2006@gmail.com>
2025-08-01 00:05:10 +0100

    I performed some tasks on functions to aid in better understanding of the functions concept

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+'''
 # Task: Asks the user to input a number and determines if it is even or odd
 def is_even():
 	while True:
@@ -14,3 +15,53 @@ def is_even():
 				print(f"'{user_input}'is not a valid whole number.")
 # call the function
 is_even()
+
+
+# NOTES!!!
+#Try/Except Block
+#Loops while-loop
+#Functions 
+#Conditionals
+#Operators
+#A function is just a set of instructions. a function musn't define anything, it can also execute a set of instruction which you musn't always write
+'''
+
+'''
+#Task 2: Ask the user for two numbers and calculate their sum
+def is_sum():
+	while True:
+		user_input1 = input("Please enter any number: ")
+	
+		user_input2 = input("Please enter any number: ")
+	
+		number1 = int(user_input1)
+	
+		number2 = int(user_input2)
+	
+		total_sum = number1 + number2
+		print(f"The total sum of {number1} and {number2} is {total_sum}.")
+is_sum()
+'''
+
+#Task 3: Ask the user to input a number and find determine if it's positive, negative or zero
+def check_number_sign():
+	while True:
+		try:
+			user_input = input("Please enter any number: ")
+	
+			number = int(user_input)
+
+			if number > 0:
+				print(f"The number {number} is Positive.")
+			elif number < 0:
+				print(f"The number {number} is Negative")
+			else:
+				print(f"The number {number} is Zero")
+		except ValueError: 
+			print(f"The value {user_input} is not a valid number.")
+
+check_number_sign() 
+
+
+
+
